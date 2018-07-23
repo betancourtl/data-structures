57a431926fc14f75cc06752d3641a8b3d4a3202a
Luis Betancourt <webdeveloperpr@gmail.com>
2018-07-23 15:24:15 -0400

    Finish doubly linked lists

--- src/DoublyLinkedList/index.js
+++ src/DoublyLinkedList/index.js
@@ -23,6 +23,68 @@ class DoublyLinkedList extends LinkedList {
     this.count++;
     this.tail = node;
   };
+
+  insertAt = (index, el) => {
+    if (!this._withinBoundary(index)) return undefined;
+    const node = new DoublyNode(el);
+    let prev = null;
+    let current = this.head;
+
+    // prev -> node -> current
+    if (index === 0) {
+      node.next = current;
+      node.prev = null;
+      current.prev = node;
+      this.head = node;
+    }
+
+    // element is not at head
+    else {
+      let i = 0;
+      while (i < index) {
+        prev = current;
+        current = current.next;
+        i++;
+      }
+
+      // prev -> node -> current
+      prev.next = node;
+      node.prev = prev;
+      node.next = current;
+      current.prev = node;
+    }
+    this.count++;
+    this.tail = this.getElementAt(this.count - 1);
+  };
+
+  removeAt = (index) => {
+    if (!this._withinBoundary(index)) return undefined;
+    let current = this.head;
+
+    if (index === 0) {
+      this.head = current.next;
+      // prev | current | next
+      if (current.next && current.next.prev) current.next.prev = null;
+      else this.tail = null;
+      this.count--;
+      return;
+    }
+
+    let prev = null;
+    for (let i = 0; i < index; i++) {
+      // prev | current | next
+      //        ^ to be deleted
+      prev = current;
+      current = current.next;
+    }
+    prev.next = current.next;
+
+    if (prev.next && prev.next.prev) prev.next.prev = prev;
+    else this.tail = prev;
+
+    this.count--;
+    return current.element;
+  };
 }
 
 export default DoublyLinkedList;
--- src/DoublyLinkedList/index.spec.js
+++ src/DoublyLinkedList/index.spec.js
@@ -1,6 +1,6 @@
 import DoublyLinkedList from './';
 
-describe('DoublyLinkedList', () => {
+describe.only('DoublyLinkedList', () => {
   it('should push 1 item to a linked list', () => {
     const list = new DoublyLinkedList();
     list.push(1);
@@ -30,5 +30,129 @@ describe('DoublyLinkedList', () => {
       },
     });
     expect(list.tail).to.equal(list.getElementAt(2));
+  });
+  it('should insert item at head', () => {
+    const list = new DoublyLinkedList();
+    list.push(2);
+    list.insertAt(0, 1);
+    expect(list.head).to.deep.equal({
+      element: 1,
+      prev: null,
+      next: {
+        element: 2,
+        prev: list.getElementAt(0),
+        next: null,
+      }
+    });
+    expect(list.tail).to.deep.equal(list.getElementAt(1));
+  });
+  it('should insert item at non-head', () => {
+    const list = new DoublyLinkedList();
+    list.push(1);
+    list.push(3);
+    list.push(4);
+    list.insertAt(1, 2);
+    expect(list.head).to.deep.equal({
+      element: 1,
+      prev: null,
+      next: {
+        element: 2,
+        prev: list.getElementAt(0),
+        next: {
+          element: 3,
+          prev: list.getElementAt(1),
+          next: {
+            element: 4,
+            prev: list.getElementAt(2),
+            next: null,
+          }
+        }
+      }
+    });
+
+  });
+  it('should insert item at tail', () => {
+    const list = new DoublyLinkedList();
+    list.push(1);
+    list.push(2);
+    list.push(4);
+    list.insertAt(2, 3);
+    expect(list.head).to.deep.equal({
+      element: 1,
+      prev: null,
+      next: {
+        element: 2,
+        prev: list.getElementAt(0),
+        next: {
+          element: 3,
+          prev: list.getElementAt(1),
+          next: {
+            element: 4,
+            prev: list.getElementAt(2),
+            next: null,
+          }
+        }
+      }
+    });
+    expect(list.tail).to.deep.equal({
+      element: 4,
+      prev: list.getElementAt(2),
+      next: null,
+    })
+  });
+  it('should should remove a head node', () => {
+    const list = new DoublyLinkedList();
+    list.push(1);
+    list.push(2);
+    list.removeAt(0);
+    expect(list.head).to.deep.equal({
+      element: 2,
+      next: null,
+      prev: null,
+    });
+    expect(list.tail.element).to.equal(2);
+  });
+  it('should should remove a non-head node', () => {
+    const list = new DoublyLinkedList();
+    list.push(1);
+    list.push(2); // to be removed
+    list.push(3);
+    list.removeAt(1);
+    expect(list.getElementAt(0).element).to.equal(1);
+    expect(list.getElementAt(0).prev).to.equal(null);
+    expect(list.getElementAt(0).next).to.equal(list.getElementAt(1));
+
+    expect(list.getElementAt(1).element).to.equal(3);
+    expect(list.getElementAt(1).prev).to.equal(list.getElementAt(0)); // error
+    expect(list.getElementAt(1).next).to.equal(null);
+    expect(list.tail.element).to.equal(3);
+  });
+  it('should should remove a tail and update tail', () => {
+    const list = new DoublyLinkedList();
+    list.push(1);
+    list.push(2);
+    list.push(3); // to be removed
+    list.removeAt(2);
+    expect(list.getElementAt(0).element).to.equal(1);
+    expect(list.getElementAt(0).prev).to.equal(null);
+    expect(list.getElementAt(0).next).to.equal(list.getElementAt(1));
+
+    expect(list.getElementAt(1).element).to.equal(2);
+    expect(list.getElementAt(1).prev).to.equal(list.getElementAt(0)); // error
+    expect(list.getElementAt(1).next).to.equal(null);
+    expect(list.tail.element).to.equal(2);
+  });
+  it('should remove the only element and update tail', () => {
+    const list = new DoublyLinkedList();
+    list.push(1);
+    list.removeAt(0);
+    expect(list.tail).to.equal(null);
+  });
+  it('should remove an element', () => {
+    const list = new DoublyLinkedList();
+    list.push(1);
+    list.push(2);
+    list.remove(1);
+    expect(list.getElementAt(0).element).to.equal(2);
   });
 });
